d8b77a178b0b815fd4f65486f98d81d0135cfd39
Ben Ong <ben_ong@dlsu.edu.ph>
2023-10-13 17:07:59 +0800

    Added Login/Register Functionality

--- app/src/main/java/com/mobdeve/see/fitnessapp/LoginActivity.kt
+++ app/src/main/java/com/mobdeve/see/fitnessapp/LoginActivity.kt
@@ -1,20 +1,51 @@
 package com.mobdeve.see.fitnessapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mobdeve.see.fitnessapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    // Replace with a more secure storage method for production
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewBinding: ActivityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        viewBinding.loginButton.setOnClickListener {
-            val intent = Intent(applicationContext, UserProfileActivity::class.java)
-            this.startActivity(intent)
-            finish()
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        loginButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                /*val storedEmail = sharedPreferences.getString("email", "")
+                val storedPassword = sharedPreferences.getString("password", "")
+
+                if (email == storedEmail && password == storedPassword) {
+                    // Success
+                    startActivity(Intent(this, UserProfileActivity::class.java))
+                } else {
+                    // Failure
+                    Toast.makeText(this, "Invalid email or password", Toast.LENGTH_SHORT).show()
+                }*/
+                val intent = Intent(applicationContext, UserProfileActivity::class.java)
+                this.startActivity(intent)
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/com/mobdeve/see/fitnessapp/RegisterActivity.kt
+++ app/src/main/java/com/mobdeve/see/fitnessapp/RegisterActivity.kt
@@ -1,20 +1,52 @@
 package com.mobdeve.see.fitnessapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.mobdeve.see.fitnessapp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var fullNameEditText: EditText
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+
+    // Replace with a more secure storage method for production
+    private lateinit var sharedPreferences: SharedPreferences
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewBinding: ActivityRegisterBinding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        viewBinding.registerButton.setOnClickListener {
-            val intent = Intent(applicationContext, UserProfileActivity::class.java)
-            this.startActivity(intent)
-            finish()
+        fullNameEditText = findViewById(R.id.fullNameEditText)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+
+        val registerButton: Button = findViewById(R.id.registerButton)
+
+        registerButton.setOnClickListener {
+            val fullName = fullNameEditText.text.toString()
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (fullName.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                //
+                /*val editor = sharedPreferences.edit()
+                editor.putString("fullName", fullName)
+                editor.putString("email", email)
+                editor.putString("password", password)
+                editor.apply()*/
+
+                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
+                val intent = Intent(applicationContext, UserProfileActivity::class.java)
+                this.startActivity(intent)
+                finish()
+            }
         }
     }
 }
